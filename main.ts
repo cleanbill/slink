f86bcd552d584fc11f472cfc8e245349dfb27e54
Mick Carter <cick.marter@gmail.com>
2024-05-28 14:14:47 +0100

    With API Key

--- main.ts
+++ main.ts
@@ -1,4 +1,8 @@
-import { Hono } from "https://deno.land/x/hono@v3.4.1/mod.ts";
+import { Context, Hono } from "https://deno.land/x/hono@v3.4.1/mod.ts";
+import { load } from "https://deno.land/std@0.224.0/dotenv/mod.ts";
+
+const env = await load();
+const key = env["SLINK_API_KEY"];
 
 export const app = new Hono();
 let kv;
@@ -20,19 +24,34 @@ try {
   }
 }
 
-// Redirect root URL
 app.get("/", (c) => c.redirect("/locals/main"));
 
-// Create a local (POST body is JSON)
-app.post("/locals", async (c) => {
+const checkApiKey = (c: Context) => {
+  const token = c.req.param("token");
+  if (key != token) {
+    c.status(403);
+    return c.body("Bad api key");
+  }
+  return null;
+
+}
+
+app.post("/locals/:token", async (c: Context) => {
+  const apiKeyResponse = checkApiKey(c);
+  if (apiKeyResponse) {
+    return apiKeyResponse;
+  }
   const body = await c.req.json();
   body.dtm = new Date().getTime();
   const result = await kv.set(["locals", body.token], body);
   return c.json(result);
 });
 
-// Get a local by title
 app.get("/locals/:token", async (c) => {
+  const apiKeyResponse = checkApiKey(c);
+  if (apiKeyResponse) {
+    return apiKeyResponse;
+  }
   const token = c.req.param("token");
   const result = await kv.get(["locals", token]);
   console.log("'", token, '" request got ', result);
