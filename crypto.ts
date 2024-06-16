33dc4fce71814abf557d461d5762568f29a59322
Mick Carter <cick.marter@gmail.com>
2024-06-16 17:49:44 +0100

    Try to force key length

--- crypto.ts
+++ crypto.ts
@@ -2,11 +2,16 @@ import { Aes } from "https://deno.land/x/crypto/aes.ts";
 import { Cbc, Padding } from "https://deno.land/x/crypto/block-modes.ts";
 
 const filla = '1UJOIU--HHT"c-e3--&NJJSSSS0';
-const fillTo16 = (s: string): string => s.length > 16 ? s.substring(0, 16) : fillTo16(s + filla.substring(s.length, s.length + 1));
+const fillTo15 = (s: string): string => s.length > 15 ? s.substring(0, 15) : fillTo15(s + filla.substring(s.length, s.length + 1));
+const tobytes32 = (s: string) => {
+    const key = fillTo15(s);
+    const buf = key.padEnd(32, "\0");
+    return buf;
+}
 
 const encrypt = (password: string, dataString: string) => {
     const te = new TextEncoder();
-    const pw = fillTo16(password);
+    const pw = tobytes32(password);
     const key = te.encode(pw);
     const data = te.encode(dataString);
     const iv = new Uint8Array(16);
@@ -17,7 +22,7 @@ const encrypt = (password: string, dataString: string) => {
 
 const decipher = (password: string, encrypted: Uint8Array) => {
     const te = new TextEncoder();
-    const pw = fillTo16(password);
+    const pw = tobytes32(password);
     const key = te.encode(pw);
     const iv = new Uint8Array(16);
     const decipher = new Cbc(Aes, key, iv, Padding.PKCS7);
